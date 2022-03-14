8140f22e459a308d34c7e61ff612e447824b466c
Seetharaman GR <grsind19@gmail.com>
2022-03-14 10:54:26 +0530

    first commit

---
+++ Intro/Addition.java
@@ -0,0 +1,22 @@
+import java.util.*;
+//This is a addition class,used to add two numbers
+class Addition {
+    public static void main(String[] args) {
+        //This is the scanner input
+        Scanner scan = new Scanner(System.in);
+        //asking the user to give input
+        System.out.println("Enter the number1");
+        int num1 = scan.nextInt();
+        //asking the user to give input
+        System.out.println("Enter the number2");
+        int num2 = scan.nextInt();
+        //complete the addition
+        int num3 = num1+num2;
+        //print the result
+        System.out.println("The Result is "+num1);
+
+        //Assignment ops
+        //Arithmatic ops
+    }
+}
+
